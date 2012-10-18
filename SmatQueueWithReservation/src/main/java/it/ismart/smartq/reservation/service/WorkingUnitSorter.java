c30b19ff0c23945e255ef03cecd2fb9356ab65e7
mancilla.rosella@gmail.com <mancilla.rosella@gmail.com@3af11777-5d79-619e-b280-08448b876799>
2012-10-18 15:00:19 +0000

    WorkingUnitSorter class added.

---
+++ SmatQueueWithReservation/src/main/java/it/ismart/smartq/reservation/service/WorkingUnitSorter.java
@@ -0,0 +1,28 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) I-Smart S.R.L., 2012
+ *
+ * This unpublished material is proprietary to I-Smart S.R.L.
+ * All rights reserved. The methods and techniques described
+ * herein are considered trade secrets and/or confidential.
+ * Reproduction or distribution, in whole  or in part, is
+ * forbidden except by express written permission of I-Smart S.R.L.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package it.ismart.smartq.reservation.service;
+
+/**
+ * 
+ * This class uses the WorkingUnits retrieved from the WorkingUnitFacade to
+ * calculate next WorkingUnit to serve. It create a serving order of the
+ * WoringUnits available.
+ * 
+ * @date $Date$
+ * @revision $Revision$
+ * @author $Author$
+ * 
+ *         File : $HeadURL$
+ * 
+ */
+public class WorkingUnitSorter {
+
+}
